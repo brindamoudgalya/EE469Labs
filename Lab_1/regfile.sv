344a073411da5e7f4828b3803b49aac39d6a2c56
Brinda Moudgalya <brindam@Brindas-MacBook-Air.local>
2026-04-04 17:35:11 -0700

    all sv files i think, BEFORE testbenching

---
+++ Lab_1/regfile.sv
@@ -0,0 +1,24 @@
+module regfile (ReadData1, ReadData2, ReadRegister1, ReadRegister2, WriteRegister, WriteData, RegWrite, clk, reset);
+    output [63:0] ReadData1, ReadData2;
+    input [4:0] ReadRegister1, ReadRegister2, WriteRegister;
+    input [63:0] WriteData;
+    input RegWrite, clk, reset;
+
+    logic [31:0] writeEn;
+    logic [31:0][63:0] regOut;
+
+    dec5to32 d(writeEn, WriteRegister, RegWrite);
+
+    genvar i;
+    generate
+        for (i=0; i < 31; i++) begin : register
+            reg64 r(regOut[i], WriteData, writeEn[i], clk, reset);
+        end
+    endgenerate
+
+    assign regOut[31] = 64'b0;
+
+    mux64x32to1 m1(ReadData1, reg_out, ReadRegister1);
+    mux64x32to1 m2(ReadData2, reg_out, ReadRegister2);
+
+endmodule
