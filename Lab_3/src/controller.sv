41a90ed0b0de50954c8d85d55741c9b186c11838
Brinda Moudgalya <brindam@Brindas-MacBook-Air.local>
2026-04-18 15:47:50 -0700

    new

--- Lab_3/src/controller.sv
+++ Lab_3/src/controller.sv
@@ -1,4 +1,7 @@
 module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, RegToLoc, ALUOp, MemToReg, Branch, UncondBranch);
+    input logic [31:0] instruction;
+    output logic ALUSource, RegWrite, MemRead, MemWrite, RegToLoc;
+    output logic [1:0] ALUOp;
 
 
 endmodule
