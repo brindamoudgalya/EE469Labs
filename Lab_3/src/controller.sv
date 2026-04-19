e189db0c7c41c72e1bbc8eedd74acaa6f07ea304
Brinda Moudgalya <brindamoudgalya05@gmail.com>
2026-04-18 20:45:23 -0700

    i think done with the casez statement??

--- Lab_3/src/controller.sv
+++ Lab_3/src/controller.sv
@@ -1,8 +1,9 @@
-module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, RegToLoc, ALUOp, MemToReg, Branch, UncondBranch);
+module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, Reg2Loc, ALUOp, MemToReg, Branch, UncondBranch, SetFlags, ImmSel);
     input logic [31:0] instruction;
-    output logic ALUSource, RegWrite, MemRead, MemWrite, RegToLoc, Branch, UncondBranch, SetFlags;
+    output logic ALUSource, RegWrite, MemRead, MemWrite, Reg2Loc, Branch, UncondBranch, SetFlags;
     output logic [1:0] ALUOp, MemToReg, ImmSel;
 
+    // ALUOp:       00: Memory      01: [C]Branch   10: Data processing
     // ImmSel:      00: I-type      01: D-type      10: B-type      11: CB-type
 
     logic [10:0] opcode;
@@ -10,22 +11,23 @@ module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, RegToLoc
 
     always_comb begin
         // defaults
-        RegToLoc = 0; ALUSource = 0; Branch = 0; RegWrite = 0; 
+        Reg2Loc = 0; ALUSource = 0; Branch = 0; RegWrite = 0; 
         MemRead = 0; MemWrite = 0; UncondBranch = 0; SetFlags = 0;
         MemToReg = 2'b0; ALUOp = 2'b0; ImmSel = 2'b0;
 
         // casez : case statement with don't cares = z or ?
+        // TODO: FIX THESE
         casez (opcode)
             // ADDI
             11'b1001000100x: begin
-                ImmSel = 2'b00;
-                ALUOp = 2'b; // TODO: FIX THESE
+                ImmSel = 2'b00; ALUSource = 1; MemToReg = 2'b00;
+                ALUOp = 2'b10;
             end
 
             // ADDS
             11'b10101011000: begin
-                SetFlags = 1;
-                ALUOp = 2'b;
+                SetFlags = 1; MemToReg = 2'b00;
+                ALUOp = 2'b10;
             end
 
             // B
@@ -35,13 +37,13 @@ module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, RegToLoc
 
             // B.LT
             11'b01010100xxx: begin
-                Branch = 1; ImmSel = 2'b11;
-                ALUOp = 2'b;
+                Branch = 1; ImmSel = 2'b11; Reg2Loc = 1;
+                ALUOp = 2'b01;
             end
 
             // BL
             11'b100101xxxxx: begin
-                UncondBranch = 1; ImmSel = 2'b10;
+                UncondBranch = 1; ImmSel = 2'b10; MemToReg = 2'b10;
             end
 
             // BR
@@ -51,23 +53,26 @@ module controller (instruction, ALUSource, RegWrite, MemRead, MemWrite, RegToLoc
 
             // CBZ
             11'b10110100xxx: begin
-                Branch = 1; ImmSel = 2'b11;
-                ALUOp = 2'b;
+                Branch = 1; ImmSel = 2'b11; Reg2Loc = 1;
+                ALUOp = 2'b01;
             end
 
             // LDUR
             11'b11111000010: begin
-
+                ALUSource = 1; MemToReg = 2'b01; MemRead = 1;
+                ALUOp = 2'b00;
             end
 
             // STUR
             11'b11111000000: begin
-
+                ALUSource = 1; Reg2Loc = 1; MemWrite = 1;
+                ALUOp = 2'b00;
             end
 
             // SUBS
             11'b11101011000: begin
-
+                SetFlags = 1; MemToReg = 2'b00;
+                ALUOp = 2'b10;
             end
         endcase
     end
